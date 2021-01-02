ff0da09f7bb313c58625095d9900a2616ba2a9c9
hjdeepsleep <hjdeepsleep@gmail.com>
2021-01-02 16:47:02 +0900

    test : 피보나치 테스트 수정

--- src/test/java/com/github/hjdeepsleep/basic/FibonacciTest.java
+++ src/test/java/com/github/hjdeepsleep/basic/FibonacciTest.java
@@ -2,20 +2,22 @@ package com.github.hjdeepsleep.basic;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class FibonacciTest {
 
     @DisplayName("n번째 피보나치 수 구하기")
-    @Test
-    public void fibonacci() throws Exception {
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "3:2", "7:13", "10:55"}, delimiter = ':')
+    public void fibonacci(int n, int expect) throws Exception {
         //given
         Fibonacci al = new Fibonacci();
 
         //when
-        int result = al.fibonacci(10);
+        int result = al.fibonacci(n);
 
         //then
-        Assertions.assertTrue(result == 55);
+        Assertions.assertTrue(result == expect);
     }
 }
